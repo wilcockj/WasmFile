4fbc0f65e1b687c10e2c7be1afc8f36c51d99efb
James Wilcock <jameswilcock8@gmail.com>
2024-03-19 21:56:36 -0700

    changed name in build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-emcc -o hello3.html \
-	hello3.c --shell-file html_template/shell_minimal.html \
+emcc -o demo.html \
+	demo.c --shell-file html_template/shell_minimal.html \
 	-s NO_EXIT_RUNTIME=1 \
 	-s "EXPORTED_RUNTIME_METHODS=['ccall','cwrap']"
